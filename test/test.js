00a91e33c3e45d9b678d7907f57454f6763af888
Maurizio De Santis <m.desantis@morganspa.com>
2013-12-04 10:08:16 +0100

    Refactoring: test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var contents = "\nABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\nABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\nABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\nABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\n\n";
+  var contents = $('#sample').text();
   $('#test1').textFadeIn(          { 'milliseconds': 1  });
   $('#test2').textFadeIn(contents, { 'milliseconds': 1  });
   $('#test3').textFadeIn(contents, { 'milliseconds': 10 });
