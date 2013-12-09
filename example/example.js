33c252717fd47a675e811c5037b8709e06885387
Maurizio De Santis <m.desantis@morganspa.com>
2013-12-09 15:13:57 +0100

    Improve examples

--- example/example.js
+++ example/example.js
@@ -14,25 +14,26 @@ $(document).ready(function() {
   $('#test1i').on('complete.textFadeIn', function(e) {
     console.log(e.type+'.'+e.namespace, e)
   })
-  $('#test1i').textFadeIn({                   'milliseconds': 1                         })
-  $('#test2i').textFadeIn({ 'text': contents, 'milliseconds': 1                         })
+  $('#test1i').textFadeIn({                   'milliseconds': 10                        })
+  $('#test2i').textFadeIn({ 'text': contents, 'milliseconds': 10                        })
   $('#test3i').textFadeIn({ 'text': contents, 'milliseconds': 10                        })
   $('#test4i').textFadeIn({ 'text': contents, 'threads':      3                         })
   $('#test5i').textFadeIn({ 'text': contents,
-    'milliseconds': 10,
+    'milliseconds': 20,
     'threads': 3
   })
-  $('#test6i').textFadeIn({ 'text': contents, 'milliseconds': 1,  'sequence': 'ltr_ttb' })
-  $('#test6i').textFadeIn({ 'text': contents, 'milliseconds': 1,  'sequence': 'ltr_btt' })
+  $('#test6i').textFadeIn({ 'text': contents, 'milliseconds': 10,  'sequence': 'ltr_ttb' })
+  $('#test7i').textFadeIn({ 'text': contents, 'milliseconds': 10,  'sequence': 'ltr_btt' })
 
-  $('#test1o').textFadeOut({                   'milliseconds': 1                         })
-  $('#test2o').textFadeOut({ 'text': contents, 'milliseconds': 1                         })
+  $('#test1o').textFadeOut({                   'milliseconds': 10                        })
+  $('#test2o').textFadeOut({ 'text': contents, 'milliseconds': 10                        })
   $('#test3o').textFadeOut({ 'text': contents, 'milliseconds': 10                        })
   $('#test4o').textFadeOut({ 'text': contents, 'threads':      3                         })
   $('#test5o').textFadeOut({ 'text': contents,
-    'milliseconds': 10,
+    'milliseconds': 20,
     'threads': 3
   })
-  $('#test6o').textFadeOut({ 'text': contents, 'milliseconds': 1,  'sequence': 'ltr_btt' })
+  $('#test6o').textFadeOut({ 'text': contents, 'milliseconds': 10,  'sequence': 'ltr_ttb' })
+  $('#test7o').textFadeOut({ 'text': contents, 'milliseconds': 10,  'sequence': 'ltr_btt' })
 
 })
